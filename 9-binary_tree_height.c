6d0c501990fd08e893c093e54896c17c6294e3cc
JCRoooD <juancarlos-99@live.com>
2023-09-18 16:37:13 -0400

    fiexed height funct

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -10,15 +10,16 @@ size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t left_height, right_height;
 
-	if (tree == NULL)
-		return (0); /* If tree is NULL, return 0 */
+	if (tree)
+	{
+		left_height = 0;
+		right_height = 0;
 
-	/* Recursively calculate the height of the left subtree */
-	left_height = binary_tree_height(tree->left);
-
-	/* Recursively calculate the height of the right subtree */
-	right_height = binary_tree_height(tree->right);
-
-	/* Return the greater of the two heights, plus 1 for the current node */
-	return (left_height > right_height ? left_height + 1 : right_height + 1);
+	/* compute the depth of each subtree */
+	left_height = (tree->left) ? binary_tree_height(tree->left) + 1 : 0;
+	right_height = (tree->right) ? binary_tree_height(tree->right) + 1 : 0;
+	/*return the corresponding height*/
+	return ((left_height > right_height) ? left_height : right_height);
+	}
+	return (0);
 }
